9ce52c55d70f1dd1eab8626bdb5ba0649b52743f
sameermalik816830 <sameermalik816830@gmail.com>
2025-11-16 11:21:04 +0530

    Adding add button to add new task

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 const input=document.getElementById("enter-task");
 const addBtn=document.querySelector("button");
+const taskContainer=document.querySelector("ul");
+
+
 
 
 let taskId=localStorage.length;
+
+
+
+
+addBtn.addEventListener("click",(e)=>{
+    if(input.textContent.trim()!=="")
+    {
+        localStorage.setItem(taskId,input.textContent);
+        taskId++;
+        const li=document.createElement("li");
+        li.textContent=input.textContent;
+        taskContainer.append(li);
+    }
+})
+
+document.body.addEventListener("keydown",(e)=>{
+    if(e.key==="Enter")
+    {
+        localStorage.setItem(taskId,input.textContent);
+        taskId++;
+    }
+})
