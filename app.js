03f8296a01da5957f7d0c7630a2fcf779d551a67
sameermalik816830 <sameermalik816830@gmail.com>
2025-11-17 21:19:28 +0530

    Implement delete button

--- app.js
+++ app.js
@@ -2,38 +2,90 @@ const input=document.getElementById("enter-task");
 const addBtn=document.querySelector(".btn");
 const taskContainer=document.querySelector("ul");
 
+const tasks=JSON.parse(localStorage.getItem("task")) || []
 
-class EditAndDelBtn{
-    constructor(text,taskId){
-        const li = document.createElement("li");
-        const div = document.createElement("div");
-        const editBtn = document.createElement("button");
-        const deleteBtn = document.createElement("button");
-        editBtn.textContent="Edit";
-        deleteBtn.textContent="Delete";
-        editBtn.setAttribute("class","edit btn");
-        deleteBtn.setAttribute("class","delete btn");
-        div.append(editBtn);
-        div.append(deleteBtn);
-        li.textContent = text;
-        li.append(div);
-        li.setAttribute("id",taskId);
-        this.task=li;
+function truncate(text){
+    if(text.length<=20)
+    {
+        return text;
+    }
+    return text.substring(0,20)+"...";
+}
+
+for(let task of tasks)
+{
+    const text=truncate(task.task);
+    const li=createLi(text,task.id);
+    taskContainer.append(li);
+}
+
+
+function createLi(text,taskId){
+    const li = document.createElement("li");
+    const div = document.createElement("div");
+    const editBtn = document.createElement("button");
+    const deleteBtn = document.createElement("button");
+    editBtn.textContent="Edit";
+    deleteBtn.textContent="Delete";
+    editBtn.setAttribute("class","edit btn");
+    deleteBtn.setAttribute("class","delete btn");
+    div.append(editBtn);
+    div.append(deleteBtn);
+    li.textContent = text;
+    li.append(div);
+    li.setAttribute("id",taskId);
+    return li;
+}
+
+
+class NewTask{
+    constructor(text,id)
+    {
+        this.id=id;
+        this.task=text;
     }
 }
 
-let taskId=localStorage.length;
-for(let i=0; i<taskId; i++)
-{
-    const newTask=new EditAndDelBtn(localStorage.getItem(i),i);
-    taskContainer.append(newTask.task);
-}
 
 
 document.body.addEventListener("click",(e)=>{
-    console.log(e.target);
+
+    if(e.target.localName==="button" && e.target.textContent==="Edit")
+    {
+        console.dir(e.target);
+        editTask(e.target.parentElement.parentElement.getAttribute("id"));
+    }
+    else if(e.target.localName==="button" && e.target.textContent==="Delete"){
+        deleteTask(e.target.parentElement.parentElement.getAttribute("id"));
+    }
 })
 
+
+const deleteTask=(id)=>{
+    console.log(id);
+    const tasks=JSON.parse(localStorage.getItem("task")) || [];
+    for(let i=0; i<tasks.length; i++)
+    {
+        if(tasks[i].id==id)
+        {
+            tasks.splice(i,1);
+            localStorage.setItem("task",JSON.stringify(tasks));
+            break;
+        }
+    }
+    const allTask=document.querySelectorAll("li");
+    for(let task of allTask){
+        if(task.getAttribute("id")===id)
+        {
+            task.remove();
+            break;
+        }
+    }
+}
+
+const editTask=(id)=>{
+    console.log(id);
+}
 
 
 addBtn.addEventListener("click",(e)=>{
@@ -52,10 +104,16 @@ document.body.addEventListener("keydown",(e)=>{
 
 
 const addTask=()=>{
-    localStorage.setItem(taskId,input.value);
-    const newTask=new EditAndDelBtn(input.value,taskId);
-    taskContainer.append(newTask.task);
+    const newTaskId=Date.now();
+    const text=input.value;
+    const newTask=new NewTask(text,newTaskId);
+    const tasks=JSON.parse(localStorage.getItem("task")) || [];
+    tasks.push(newTask);
+    localStorage.setItem("task",JSON.stringify(tasks));
+    const li = createLi(truncate(text),newTaskId);
+    taskContainer.append(li);
     input.value="";
 }
+
 
 
