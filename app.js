ae87fbc2f2065bf1d6b7a891d0de033bf2dc9b90
sameermalik816830 <sameermalik816830@gmail.com>
2025-11-19 19:54:19 +0530

    Implement edit button

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 const input=document.getElementById("enter-task");
-const addBtn=document.querySelector(".btn");
+const addBtn=document.querySelector(".add-item");
 const taskContainer=document.querySelector("ul");
+const editWindow=document.querySelector("dialog");
+const cancelBtn=document.getElementById("cancel");
+const saveBtn=document.getElementById("save");
+const editInput=document.getElementById("edit");
+const msg=document.querySelector(".container p");
+const editMsg=document.querySelector("dialog p");
+let tasks;
 
-const tasks=JSON.parse(localStorage.getItem("task")) || []
+
+const showTask=(tasks)=>{
+    for(let task of tasks)
+    {
+        const text=truncate(task.task);
+        const li=createLi(text,task.id);
+        taskContainer.append(li);
+    }
+
+}
+
+window.addEventListener("load",(e)=>{
+    tasks=JSON.parse(localStorage.getItem("task")) || [];
+    if(tasks.length===0){
+        tasks=null;
+        return;
+    }
+    showTask(tasks);
+})
 
 function truncate(text){
     if(text.length<=20)
@@ -12,12 +37,6 @@ function truncate(text){
     return text.substring(0,20)+"...";
 }
 
-for(let task of tasks)
-{
-    const text=truncate(task.task);
-    const li=createLi(text,task.id);
-    taskContainer.append(li);
-}
 
 
 function createLi(text,taskId){
@@ -25,13 +44,16 @@ function createLi(text,taskId){
     const div = document.createElement("div");
     const editBtn = document.createElement("button");
     const deleteBtn = document.createElement("button");
+    const span=document.createElement("span");
     editBtn.textContent="Edit";
     deleteBtn.textContent="Delete";
     editBtn.setAttribute("class","edit btn");
     deleteBtn.setAttribute("class","delete btn");
+    span.textContent=text;
+    
     div.append(editBtn);
     div.append(deleteBtn);
-    li.textContent = text;
+    li.append(span);
     li.append(div);
     li.setAttribute("id",taskId);
     return li;
@@ -52,8 +74,8 @@ document.body.addEventListener("click",(e)=>{
 
     if(e.target.localName==="button" && e.target.textContent==="Edit")
     {
-        console.dir(e.target);
-        editTask(e.target.parentElement.parentElement.getAttribute("id"));
+        const task=e.target.parentElement.parentElement;
+        editTask(task);
     }
     else if(e.target.localName==="button" && e.target.textContent==="Delete"){
         deleteTask(e.target.parentElement.parentElement.getAttribute("id"));
@@ -62,7 +84,9 @@ document.body.addEventListener("click",(e)=>{
 
 
 const deleteTask=(id)=>{
-    console.log(id);
+    if(!confirm("Are you sure that you want to delete the task?")){
+        return;
+    }
     const tasks=JSON.parse(localStorage.getItem("task")) || [];
     for(let i=0; i<tasks.length; i++)
     {
@@ -83,22 +107,73 @@ const deleteTask=(id)=>{
     }
 }
 
-const editTask=(id)=>{
-    console.log(id);
+
+
+
+const editTask=(task)=>{
+    const id=task.getAttribute("id");
+    const taskVal=task.firstElementChild.textContent;
+    editInput.value=taskVal;
+    localStorage.setItem("id",JSON.stringify(id));
+    editWindow.showModal();
+}
+
+cancelBtn.addEventListener("click",(e)=>{
+    editWindow.close();
+})
+saveBtn.addEventListener("click",(e)=>{
+    const editedTask=editInput.value;
+    if(editedTask.trim()!==""){
+        editMsg.setAttribute("id","hide");
+        updateTask(editedTask);
+        editWindow.close();
+    }
+    else{
+        editMsg.setAttribute("id","show");
+    }
+    
+})
+
+
+const updateTask=(editedTask)=>{
+    const allTask=JSON.parse(localStorage.getItem("task"));
+    const id=JSON.parse(localStorage.getItem("id"));
+    for(let task of allTask)
+    {
+        if(task.id==id)
+        {
+            console.log("new");
+            task.task=editedTask;
+            localStorage.setItem("task",JSON.stringify(allTask));
+            location.reload();
+            break;
+        }
+    }
+
 }
 
 
 addBtn.addEventListener("click",(e)=>{
     if(input.value.trim()!=="")
     {
+        msg.setAttribute("id","hide");
         addTask();
+    }
+    else{
+        msg.setAttribute("id","show");
     }
 })
 
 document.body.addEventListener("keydown",(e)=>{
     if(e.key==="Enter")
     {
-        addTask();
+        if(input.value.trim()!==""){
+            msg.setAttribute("id","hide");
+            addTask();
+        }
+        else{
+            msg.setAttribute("id","show");
+        }
     }
 })
 
